e4e9653038e8a8f0f955aaf07ac2181191733ccd
Kyle GG <goliath.kylec@gmail.com>
2023-11-21 21:53:19 +0000

    arrrrrrrrrrrrrrrrrrrrrrrrrr

--- script.js
+++ script.js
@@ -88,77 +88,15 @@ var upperCasedCharacters = [
   'Z'
 ];
 
-// Function to get password length
-function isPasswordLengthInputValid(passwordInputLength) { 
-  if (passwordInputLength < 8) {
-    return false;
-  }
-  if (passwordInputLength > 128) {
-    return false;
-  }
-  if (passwordInputLength >= 8 && passwordInputLength <= 128) { 
-    return true;
-  }
-  return false;
-}
 
 // Function to prompt user for password options
 function getPasswordOptions() {
-  console.log("Get Password Options");
-
-  const passwordLength = prompt('How long should your Password be? (password should be between 8 & 128 characters)');
-  const passwordLengthAsNumber = parseInt(passwordLength, 10);
-
-  if (isPasswordLengthInputValid(passwordLengthAsNumber)) {
-    // Questions to get true or false statements for arrays above, at least 1 must be true
-    const isLowercaseIncluded = confirm('Should your password include lowercase characters?');
-    const isUppercaseIncluded = confirm('Should your password include uppercase characters?');
-    const isNumericIncluded = confirm('Should your password include numeric characters?');
-    const isSpecialCharsIncluded = confirm('Should your password include special characters?');
-
-    if (isLowercaseIncluded || isUppercaseIncluded || isNumericIncluded || isSpecialCharsIncluded) {
-    } else {
-      alert('Please choose at least 1 character type please.');
-    } 
-  } else {
-    alert('Password length invalid, password should have between 8 and 1128 charaters!')
-  }
-}
-
-  // Object to store user input information
-  let userPasswordOptions = {
-    passwordLength: passwordLengthAsNumber,
-    passwordLower: isLowercaseIncluded,
-    passwordUpper: isUppercaseIncluded,
-    passwordNumeric: isNumericIncluded,
-    passwordSpecial: isSpecialCharsIncluded
-  };
-  return userPasswordOptions;
+  
 }
 
 // Function to generate password with user input
 function generatePassword() {
-  let passwordOptions = getPasswordOptions();
-  let userPasswordConfirms = [];
-  let password = "";
-
-  if (passwordOptions.passwordLower) {
-    userPasswordConfirms = userPasswordConfirms.concat(lowerCasedCharacters);
-  }
-
-  if (passwordOptions.passwordUpper) {
-    userPasswordConfirms = userPasswordConfirms.concat(upperCasedCharacters);
-  }
-
-  if (passwordOptions.passwordNumeric) {
-    userPasswordConfirms = userPasswordConfirms.concat(numericCharacters);
-  }
-
-  if (passwordOptions.passwordSpecial) {
-    userPasswordConfirms = userPasswordConfirms.concat(specialCharacters);
-  }
-
-
+  
 }
 
 
