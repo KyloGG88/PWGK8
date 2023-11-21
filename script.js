212651bc42407f83fc93f72a9330bfd01adfb7f2
Kyle GG <goliath.kylec@gmail.com>
2023-11-21 22:12:24 +0000

    rework 2.0

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Array of special characters to be included in password
-var specialCharacters = [
+let specialCharacters = [
   '@',
   '%',
   '+',
@@ -26,10 +26,10 @@ var specialCharacters = [
 ];
 
 // Array of numeric characters to be included in password
-var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+let numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
 // Array of lowercase characters to be included in password
-var lowerCasedCharacters = [
+let lowerCasedCharacters = [
   'a',
   'b',
   'c',
@@ -59,7 +59,7 @@ var lowerCasedCharacters = [
 ];
 
 // Array of uppercase characters to be included in password
-var upperCasedCharacters = [
+let upperCasedCharacters = [
   'A',
   'B',
   'C',
@@ -89,23 +89,37 @@ var upperCasedCharacters = [
 ];
 
 
-// Function to prompt user for password options
-function getPasswordOptions() {
-  
-}
-
-// Function to generate password with user input
 function generatePassword() {
-  
+  let shouldUseLower = true;
+  let shouldUseUpper = true;
+  let shouldUseNumber = true;
+  let shouldUseSpecial = true;
+  let passwordCharacters = [];
+
+  let howLong = prompt("How long would you like the password to be? (Must be between 8 and 128 characters)");
+
+  // converting the string answer to a number so can be used later
+  let lengthOfPassword = Number(howLong);
+
+  // making sure the number is between 10 and 64 before the other questions are asked.
+  if (lengthOfPassword >= 8 && lengthOfPassword <= 128) {
+    shouldUseLower = confirm("Would you like lower case characters in your password?")
+    shouldUseUpper = confirm("Would you like upper case characters in your password?");
+    shouldUseNumber = confirm("Would you like numbers in your password?");
+    shouldUseSpecial = confirm("Would you like special characters in your password?");
+  } else {
+    alert("Invalid password length. Please enter a number between 8 and 128.");
+    return;
+  }
+
 }
 
-
-// Get references to the #generate element
+// // // Get references to the #generate element
 var generateBtn = document.querySelector('#generate');
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  let password = generatePassword();
   var passwordText = document.querySelector('#password');
 
   passwordText.value = password;
